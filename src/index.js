9ef1aa63477d03a26b153bf74dc07ec8cc7d0100
Danny Hemphill <djdand1@gmail.com>
2021-07-08 22:58:25 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Grid Column Example</h1>
+<div>
+  This example will show how to use different aspects of CSS Grid.
+</div>
+<br />
+`;
